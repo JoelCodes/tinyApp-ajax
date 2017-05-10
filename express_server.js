5fd17940a8597e2dfa91637d88105b633bcc80a8
Alexandra Sia <alexandrasia@gmail.com>
2017-05-10 19:53:45 +0000

    Added delete URL button and update long URL button

--- express_server.js
+++ express_server.js
@@ -2,16 +2,15 @@ const express = require("express");
 const app = express();
 
 const PORT = process.env.PORT || 8080;
-
-app.set("view engine", "ejs");
-
 const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
 
 function generateRandomString() {
   let shortURL = "";
@@ -30,10 +29,8 @@ app.get("/urls/new", (req, res) => {
 app.post("/urls", (req, res) => {
   const shortURL = generateRandomString();
   urlDatabase[shortURL] = req.body.longURL;
-
   res.redirect('/urls/' + shortURL);
 });
-
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
@@ -48,6 +45,16 @@ app.get("/urls/:id", (req, res) => {
   let shortURL = req.params.id;
   let templateVars = { shortURL: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_shows", templateVars);
+});
+
+app.post("/urls/:id/delete", (req, res) => {
+  delete urlDatabase[req.params.id];
+  res.redirect("/urls");
+});
+
+app.post("/urls/:id", (req, res) => {
+  urlDatabase[req.params.id] = req.body.newLongURL;
+  res.redirect("/urls");
 });
 
 app.get("/u/:shortURL", (req, res) => {
